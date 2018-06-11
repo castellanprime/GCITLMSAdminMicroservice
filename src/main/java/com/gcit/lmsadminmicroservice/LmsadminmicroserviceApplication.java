4026e9db6a2081ad6c338996585ee1d526a125eb
iratusmachina <iratusmachina@DESKTOP-8OQ93TH.(none)>
2018-06-11 17:12:58 -0400

    Initial commit

---
+++ src/main/java/com/gcit/lmsadminmicroservice/LmsadminmicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.gcit.lmsadminmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsadminmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsadminmicroserviceApplication.class, args);
+	}
+}
